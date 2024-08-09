3b3716d6092efcfde11058c00767d5eb1b8fcaaf
Jose Meza <jozmeza@gmail.com>
2024-08-09 14:21:43 -0600

    technical skills

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
+        habilidades[0].classList.add("java");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
+        habilidades[2].classList.add("python");
+        habilidades[3].classList.add("sql");
+        habilidades[4].classList.add("office");
+        habilidades[5].classList.add("capacidad_analisis");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
         habilidades[8].classList.add("dedicacion");
